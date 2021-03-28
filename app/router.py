1b7080b682350b85bdd765a7da9f53ef8fa8e819
Zach Dingels <dingelsz3@gmail.com>
2021-03-28 18:24:56 -0400

    added homepage

--- app/router.py
+++ app/router.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, current_app, send_from_directory
 
 import config
 from src.controller import cloc, GitHubRepo
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='src/view')
 
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    print(app.static_folder)
+    return send_from_directory(app.static_folder, "index.html")
 
 
 @app.route('/api', defaults={'path': ''})
